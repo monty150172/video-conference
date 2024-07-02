beb11c01a07d9015801b7a6684fb637efb1cc084
philipp sauerlaender <philipp.sauerlaender@dci-student.org>
2024-07-02 15:16:01 +0200

    footer

---
+++ src/components/Footer.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+const Footer = () => {
+    return (
+        <div
+            style={{
+                backgroundColor: "#333",
+                color: "#fff",
+                padding: "10px",
+                textAlign: "center",
+            }}
+        >
+            <p>Footer</p>
+        </div>
+    );
+};
+
+export default Footer;
