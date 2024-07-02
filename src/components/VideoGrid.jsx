893a79807333808d9ada6e85571891fa3a032452
philipp sauerlaender <philipp.sauerlaender@dci-student.org>
2024-07-02 15:32:23 +0200

    grid überarbeitet

--- src/components/VideoGrid.jsx
+++ src/components/VideoGrid.jsx
@@ -7,16 +7,31 @@ const VideoGrid = () => {
             name: "Manuel Fahrenholz",
             imageSrc: process.env.PUBLIC_URL + "/Manuel.png",
         },
-        { name: "K Gnd", imageSrc: "" },
-        { name: "Paul Schilling", imageSrc: "" },
-        { name: "Philipp Sauerlaender", imageSrc: "" },
+        { name: "K Gnd", imageSrc: process.env.PUBLIC_URL + "./klaudia.png" },
+        {
+            name: "Paul Schilling",
+            imageSrc: process.env.PUBLIC_URL + "./paul.png",
+        },
+        {
+            name: "Philipp Sauerlaender",
+            imageSrc: process.env.PUBLIC_URL + "./philipp.png",
+        },
         {
             name: "Ralf Schmitz",
             imageSrc: process.env.PUBLIC_URL + "/ralf.png",
         },
-        { name: "Julijana Uneva", imageSrc: "" },
-        { name: "Kevin Thomas Lampe", imageSrc: "" },
-        { name: "Oscar van Velsen", imageSrc: "" },
+        {
+            name: "Julijana Uneva",
+            imageSrc: process.env.PUBLIC_URL + "./juliana.png",
+        },
+        {
+            name: "Kevin Thomas Lampe",
+            imageSrc: process.env.PUBLIC_URL + "./kevin.png",
+        },
+        {
+            name: "Oscar van Velsen",
+            imageSrc: process.env.PUBLIC_URL + "./oscar.png",
+        },
     ];
 
     return (
