6f4a36c914df9e9ce4590f6819c17a362ca5cebf
philipp sauerlaender <philipp.sauerlaender@dci-student.org>
2024-07-02 15:15:49 +0200

    Header

---
+++ src/components/Header.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+const Header = () => {
+    return (
+        <div
+            style={{
+                backgroundColor: "#FFC107",
+                padding: "10px",
+                textAlign: "center",
+            }}
+        >
+            Breakout rooms are in session
+        </div>
+    );
+};
+
+export default Header;
